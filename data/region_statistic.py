c139c74e364dd5a8d37615d4683a39ae854c252e
carlosrivera22 <carlos.rivera22@upr.edu>
2017-12-05 10:30:49 -0400

    Added stats
    added stats classes

---
+++ data/region_statistic.py
@@ -0,0 +1,28 @@
+class RegionStatisticData:
+    region_stats = [
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        }
+    ]
+
+
+    def getRegionStats(self):
+        return self.region_stats
+
---
+++ data/weekly_statistic.py
@@ -0,0 +1,32 @@
+class WeeklyStatisticData:
+    weekly_stats = [
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+        {
+            'resource_needed_id': '',
+            'resource_available_id': '',
+            'resource_resolved_id': ''
+        },
+        {
+            'resource_needed_id':'',
+            'resource_available_id':'',
+            'resource_resolved_id':''
+        },
+
+    ]
+
+    def getWeeklyStats(self):
+        return self.weekly_stats
+
+
+
+
+
