60205c545a5318f3f529b7dc9d3fc8e3efa70ca7
carlosrivera22 <carlos.rivera22@upr.edu>
2017-12-28 22:47:27 -0400

    make it better

--- main.py
+++ main.py
@@ -188,6 +188,4 @@ def getAdminById(admin_id):
     return AdminHandler().getAdminById(admin_id)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    #app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080)
