3d7dbfbe97ade805d34c0954ce6b61aaf5b94091
carlosrivera22 <carlos.rivera22@upr.edu>
2018-01-09 21:26:26 -0400

    debugging
    debugging

--- main.py
+++ main.py
@@ -71,4 +71,4 @@ def getAvailableResourcesByKeyword(keyword):
 if __name__ == '__main__':
     #app.run(debug=True, port=8080)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
