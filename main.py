27acc752aa22c2c20d91d52fd7de6815ecdadb72
carlosrivera22 <carlos.rivera22@upr.edu>
2018-01-09 20:56:26 -0400

    port change
    port change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request, render_template
 
 from handler.resource import ResourceHandler
@@ -68,4 +69,6 @@ def getAvailableResourcesByKeyword(keyword):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    #app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
